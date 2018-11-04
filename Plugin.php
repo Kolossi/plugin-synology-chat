f5db895bcb3d61fda534249822ecce2dd9df5f07
paul <git@kolossi.co.uk>
2018-11-04 21:05:49 +0000

    fix Plugin.php typo

--- Plugin.php
+++ Plugin.php
@@ -15,12 +15,12 @@ class Plugin extends Base
 {
     public function initialize()
     {
-        $this->template->hook->attach('template:config:integrations', 'synologyChat:config/integration');
-        $this->template->hook->attach('template:project:integrations', 'synologyChat:project/integration');
-        $this->template->hook->attach('template:user:integrations', 'synologyChat:user/integration');
+        $this->template->hook->attach('template:config:integrations', 'synologychat:config/integration');
+        $this->template->hook->attach('template:project:integrations', 'synologychat:project/integration');
+        $this->template->hook->attach('template:user:integrations', 'synologychat:user/integration');
 
-        $this->userNotificationTypeModel->setType('synologyChat', t('SynologyChat'), '\Kanboard\Plugin\SynologyChat\Notification\SynologyChat');
-        $this->projectNotificationTypeModel->setType('synologyChat', t('SynologyChat'), '\Kanboard\Plugin\SynologyChat\Notification\SynologyChat');
+        $this->userNotificationTypeModel->setType('synologychat', t('SynologyChat'), '\Kanboard\Plugin\SynologyChat\Notification\SynologyChat');
+        $this->projectNotificationTypeModel->setType('synologychat', t('SynologyChat'), '\Kanboard\Plugin\SynologyChat\Notification\SynologyChat');
     }
 
     public function onStartup()
